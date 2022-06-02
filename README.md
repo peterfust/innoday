55935aa88f02de5d198372ea84f46af6cac76fcc
Peter Fust <peterfust@Peters-Mac-mini.local>
2022-06-02 14:05:20 +0200

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+## What You Should Know
+- [![](https://img.shields.io/badge/Download-Offline%20SDK-orange)](https://www.dynamsoft.com/web-twain/downloads)
+- [![](https://img.shields.io/badge/Get-30--day%20FREE%20Trial%20License-blue)](https://www.dynamsoft.com/customer/license/trialLicense/?product=dwt)
+
+### Install Express
+
+Run the following script to install Express.
+
+```
+npm install
+```
+
+### Start the server
+
+We defined the web server logic in the file "app.js", to start it, run the following script:
+
+```
+node app.js
+```
+
+Note that we have enabled SSL on this server at the port 4443.
+
---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const fs = require('fs');
+const https = require('https');
+const cors = require('cors');
+const util = require('util');
+const path = require('path');
+const multer = require('multer');
+const opn = require('open');
+
+const app = express();
+// Access-Control-Allow-Origin: **any**
+app.use(cors({
+    origin: (origin, callback) => {
+        return callback(null, true);
+    }
+}));
+
+// static files
+app.use(express.static(path.join(__dirname, './')));
+app.use('/dist',express.static(path.join(__dirname, '../../dist')));
+
+let httpsServer = https.createServer({
+    key: fs.readFileSync(path.join(__dirname, './pem/Dynamsoft-WebTwain-key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, './pem/Dynamsoft-WebTwain-cert.pem'))
+}, app);
+
+let httpsPort = 4443, url='https://127.0.0.1:'+httpsPort+'/HelloWorld.html';
+
+setTimeout(function(){
+  opn(url);
+},500);
+
+httpsServer.listen(httpsPort, '127.0.0.1', () => console.log(`Page is available in ${url}`));
+
